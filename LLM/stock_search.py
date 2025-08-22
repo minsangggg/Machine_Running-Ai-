22b4f034582f5234807d473cecda36e98911024b
minsangggg <parkss6468@gmail.com>
2025-08-22 17:50:11 +0900

    .

--- LLM/stock_search.py
+++
@@ -1,5 +0,0 @@
-import meilisearch
-client=meilisearch.Client('http://127.0.0.1:7700', 'yrU4Nyoa7R5YK3okInM9OfZ4Br8T9SGz4mQC8wnJ-4Q ')
-
-def stock_search(query):
-    return client.index('nasdaq').search('appl')
